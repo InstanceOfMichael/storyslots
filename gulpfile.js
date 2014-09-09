89c8b46d0024eb76878676a81afb37486cb70bf1
David Ruttka <david@davidruttka.com>
2014-09-08 22:08:51 -0700

    Add uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 var gulp    = require('gulp'),
     jshint  = require('gulp-jshint'),
     stylish = require('jshint-stylish'),
-    concat  = require('gulp-concat');
+    concat  = require('gulp-concat'),
+    uglify  = require('gulp-uglify');
 
 gulp.task('scripts', function(){
   gulp.src('./app/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter(stylish))
     .pipe(concat('app.js'))
+    .pipe(gulp.dest('./build'))
+    .pipe(uglify())
     .pipe(gulp.dest('./'));
 });
 
